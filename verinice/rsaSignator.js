c7a9265bcffecbcfbe7cbb39c6e376334198ae1b
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2014-05-03 16:26:28 +0700

    Verinice: rsaSignator.js

---
+++ verinice/rsaSignator.js
@@ -0,0 +1,12 @@
+var BigNumber = require("./bignumber")
+
+module.exports.signCertificate = function(unsignedCertificate, callback) {
+    var result = "";
+    // Stub
+    callback(result);
+};
+
+module.exports.verifyCertificate = function(signedCertificate, callback) {
+    var result = true; // Or false maybe, dear?
+    callback(result);
+};
