ea5044d90c2b3f913669e475111cdc02e690e8da
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2014-05-03 16:25:51 +0700

    Added verinice

---
+++ verinice/application.js
@@ -0,0 +1,24 @@
+/*
+ * VeriNice simple Digital Certficate Authority
+ * 
+ * Putu Wiramaswara Widya <wiramaswara11@mhs.if.its.ac.id>
+ */
+
+var rsaSignator = require("./rsaSignator.js");
+var io = require('socket.io').listen(9999);
+
+io.sockets.on('connection', function (socket) {
+    // onSignThisCertificate
+    socket.on("signThisCertificate", function(data, fn) {
+        rsaSignator.signCertificate(data, function(result) {
+            fn(result);
+        });
+    });
+    
+    // verifyCertificate
+    socket.on("verifyCertificate", function(data, fn) {
+        rsaSingator.verifyCertificate(data, function(result) {
+            fn(result);
+        });
+    })
+});
